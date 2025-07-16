605698abc2e1a1df856bbe759796dab0fcdb0eb0
Devanand <devanandk8078@gmail.com>
2025-07-16 19:14:49 +0530

    animations

---
+++ lib/animation.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MyAnimation extends StatefulWidget {
+  const MyAnimation({super.key});
+
+  @override
+  State<MyAnimation> createState() => _MyAnimationState();
+}
+
+class _MyAnimationState extends State<MyAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late Animation<Color?> _color;
+  late Animation<Size?> _size;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    final curved = CurvedAnimation(parent: _controller, curve: Curves.easeOut);
+
+    _animation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 0.2, end: 1.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 1.0, end: 2.0), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 2.0, end: 0.5), weight: 1),
+    ]).animate(curved);
+    _color = ColorTween(
+      begin: const Color.fromARGB(255, 34, 0, 255),
+      end: const Color.fromARGB(255, 255, 0, 0),
+    ).animate(_controller);
+    _size = SizeTween(
+      begin: Size(50, 50),
+      end: Size(400, 200),
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _animation.value,
+              child: Container(
+                height: _size.value!.height,
+                width: _size.value!.width,
+                color: _color.value,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
